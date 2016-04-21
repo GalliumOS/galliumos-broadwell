28829ed7e36aa60209f03e4cdf132ecda5f97da8
Hugh Greenberg <hugegreenbug@gmail.com>
2016-04-20 23:51:18 -0600

    Improved the wifi sleep script

--- lib/systemd/system-sleep/wifi.sh
+++ lib/systemd/system-sleep/wifi.sh
@@ -4,7 +4,6 @@ case $1/$2 in
     post/*)
         /sbin/rmmod iwlmvm iwlwifi mac80211 cfg80211
 	/sbin/modprobe iwlwifi
-        systemctl restart wpa_supplicant
 	systemctl restart NetworkManager
 	;;
 esac
